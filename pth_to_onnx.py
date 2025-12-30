856c0fefd6374a78a1792353b13083f085075adb
sy <105179675+Eomseoyoung@users.noreply.github.com>
2025-12-30 17:11:52 +0900

    Update pth_to_onnx.py

--- pth_to_onnx.py
+++ pth_to_onnx.py
@@ -1,13 +1,16 @@
 import torch
 from bisenetv2 import BiSeNetV2  # 모델 클래스
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # 1) 모델 로드
 model = BiSeNetV2(num_classes=2)
 ckpt = torch.load("bisenetv2_rail3.pth", map_location=device)
 model.load_state_dict(ckpt)
 model.eval().to(device)
+
 # 2) 더미 입력
-dummy = torch.randn(1, 3, 512, 1024).to(device)
+dummy = torch.randn(1, 3, 512, 1024).to(device) # 학습할때 사용한 이미지 사이즈 입력
 # 3) ONNX로 내보내기
 torch.onnx.export(
     model, dummy, "bisenetv2.onnx",
@@ -17,4 +20,5 @@ torch.onnx.export(
     do_constant_folding=True,
     dynamic_axes={"input": {0: "batch"}, "output": {0: "batch"}}
 )
-print("✅ Saved bisenetv2.onnx")
+print("Saved bisenetv2.onnx")
+
